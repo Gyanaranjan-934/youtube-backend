f64351da1eeda1ca8ff3b87cc11dc3059d27712d
Gyana <gyanaranjansahoo509@gmail.com>
2023-12-15 11:02:11 +0530

    Backend initital setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs'
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return null;
+    } else {
+      const response = await cloudinary.uploader.upload(localFilePath, {
+        resource_type: "auto"
+      })
+      console.log("File uploaded successfully", response.url);
+      return response;
+    }
+  } catch (error) {
+    fs.unlinkSync(localFilePath)
+    console.error(error);
+    return null;
+  }
+}
+
+
+cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function (error, result) { console.log(result); });
