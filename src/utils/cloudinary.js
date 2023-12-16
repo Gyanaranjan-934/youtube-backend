a783a3413bc78fdc0a5ea1ab64d0e8919b071a9c
Gyana <gyanaranjansahoo509@gmail.com>
2023-12-16 18:01:40 +0530

    register controller completed not tested

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -27,6 +27,8 @@ const uploadOnCloudinary = async (localFilePath) => {
 }
 
 
-cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" },
-  function (error, result) { console.log(result); });
+// cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag" },
+//   function (error, result) { console.log(result); });
+
+export {uploadOnCloudinary}
